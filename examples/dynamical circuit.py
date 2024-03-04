bug fix of the sigma_z matrix calculation

examples/dynamical circuit.py
@@ -11,19 +11,42 @@ from quforge.gates import State as State
 
 device='cuda'
 
+
+class Circuit(nn.Module):
+    def __init__(self):
+        super(Circuit, self).__init__()
+
+        self.layers = nn.Sequential(
+            ops.Hadamard(index=[0, 1]),
+            ops.Rotation(mtx_id=0, j=0, k=1, index=[0, 1]),
+            ops.CNOT(control=0, target=2, N=3)
+        )
+
+        self.R = ops.Rotation(mtx_id=0, j=0, k=1, index=[0])
+
+    def forward(self, x):
+        
+        x = self.R(param=[0.5])
+        x = self.layers(x)
+
+        return x
+
+
+
 circuit = qf.Circuit(dim=2, wires=2, device=device)
+circuit.add(qf.HGate)
+circuit.add(qf.RGate, mtx_id=0)
+circuit.add(qf.CNOT)
 
-#circuit.add(qf.HGate)
-#circuit.add(qf.RGate, mtx_id=0)
-#circuit.add(qf.CNOT)
 
+
+
+circuit = qf.Circuit(dim=2, wires=2, device=device)
 circuit.H()
 circuit.R(angle=[0.5])
 circuit.CNOT()
 circuit.X(index=[1])
 circuit.SWAP()
-
-print(circuit)
 
 x = State('0-0', device=device)
 

quforge/sparse.py
@@ -17,8 +17,12 @@ def Sy(j, k, base):
     return -1j*torch.kron(base[j], base[k].T) + 1j*torch.kron(base[k], base[j].T)
 
 def Sz(j, k, base):
-
-    return torch.kron(base[j], base[j].T) - torch.kron(base[k], base[k].T) + 0*1j
+    #return torch.kron(base[j], base[j].T) - torch.kron(base[k], base[k].T) + 0*1j
+    f = (2.0/(j*(j+1)))**0.5
+    s = 0.0
+    for k in range(0, j+1):
+        s += ((-j)**delta(k,j))*torch.kron(base[k], base[k].T)
+    return f*s + 0*1j
 
 sigma = [Sx, Sy, Sz]
 