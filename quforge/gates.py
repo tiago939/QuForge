2f2755b2a0d5929ba0da8e3ef6f2981ca55b6252
lucasfriedrich97 <104332359+lucasfriedrich97@users.noreply.github.com>
2024-03-04 18:01:50 -0300

    Update gates.py
    .

--- quforge/gates.py
+++ quforge/gates.py
@@ -151,7 +151,7 @@ def Sx(j, k, base):
 def Sy(j, k, base):
     return -1j*torch.kron(base[j-1], base[k-1].T) + 1j*torch.kron(base[k-1], base[j-1].T)
 
-def Sz(j, base):
+def Sz(j,k, base):
     #return torch.kron(base[j], base[j].T) - torch.kron(base[k], base[k].T) + 0*1j
     f = (2.0/(j*(j+1)))**0.5
     s = 0.0
