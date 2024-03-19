d7094f986d3f8fd0a74f0f87426ef9f79ba50427
Lucas Friedrich <104332359+lucasfriedrich97@users.noreply.github.com>
2024-03-19 11:00:37 -0300

    add math

--- quforge/Gate.py
+++ quforge/Gate.py
@@ -4,6 +4,7 @@ import numpy as np
 from math import log as log
 import itertools
 from torch.nn.parameter import Parameter
+import math
 
 def State(dits, dim, state=None, device='cpu'):
     D = dim
