b9617e9f56e1a5a9eff6c956c03a67eb26965ef8
tiago <tiago939@gmail.com>
2025-03-12 15:14:45 -0300

    version 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quforge",
-    version="0.3.1",
+    version="0.3.2",
     author="Tiago de Souza Farias",
     author_email="tiago.farias@ufscar.br",
     description="QuForge: a library for qudit simulation",
